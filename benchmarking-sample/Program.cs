4ced73f8d527a6cb9814850822fb25ce6de54d53
hakanmarthidir <hakan_hidir@hotmail.com>
2022-06-12 17:14:59 +0200

    first commit

---
+++ benchmarking-sample/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace benchmarking_sample
+{
+    [SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net60)]
+    [MarkdownExporter, HtmlExporter]
+    public class LoopBenchmark
+    {
+        [Benchmark]
+        [Arguments(100)]
+        [Arguments(1000)]
+        [Arguments(10000)]
+        public void ForLoop(int max)
+        {
+            for (int i = 0; i < max; i++)
+            {
+            }
+        }
+
+        [Benchmark(Baseline = true)]
+        [Arguments(100)]
+        [Arguments(1000)]
+        [Arguments(10000)]
+        public void WhileLoop(int max)
+        {
+            int i = 0;
+            while (i < max)
+            {
+                i++;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
